702ce3287954bc01ad1c98e11e459433a6677502
Massimiliano Cristarella <m.cristarella@outlook.com>
2020-11-18 13:41:15 +0100

    Refactored FizzBuzz Class

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,23 +1,29 @@
 package dssc.assignment.fizzbuzz;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class FizzBuzz {
 
     public static void main(String... args) {
-        for (int i = 1; i <= 105; ++i) {
-            if (i % 3 == 0) {
-                System.out.print("Fizz");
-            }
-            if (i % 5 == 0) {
-                System.out.print("Buzz");
-            }
-            if (i % 7 == 0) {
-                System.out.print("Bang");
-            }
-            if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
-                System.out.print(i);
-            }
-            System.out.println();
-        }
+        print();
+    }
+
+    public static String convert(int n) {
+        if (!(n % 3 == 0 || n % 5 == 0 || n % 7 == 0))
+            return Integer.toString(n);
+
+        String str = "";
+        str += n % 3 == 0 ? "Fizz" : "";
+        str += n % 5 == 0 ? "Buzz" : "";
+        str += n % 7 == 0 ? "Bang" : "";
+        return str;
+    }
+
+    public static void print() {
+        Stream<String> fizzBuzzBangs = IntStream.rangeClosed(1, 105).mapToObj(FizzBuzz::convert);
+        System.out.println(fizzBuzzBangs.collect(Collectors.joining(", ")));
     }
 
 }
